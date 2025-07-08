400921c900e9eb1abbd25f05734059241b929e18
Guilherme Lessa <gui.silva.lessa@gmail.com>
2025-07-08 10:47:57 -0300

    Add initial Email List resource scaffolding
    Introduces the EmailList model, migration, factory, seeder, policy, and controller with basic resource methods. Adds views and navigation for managing email lists, and updates routes to include email list index and create pages. This sets up the foundation for CRUD operations on email lists.

--- BlastMail/routes/web.php
+++ BlastMail/routes/web.php
@@ -1,16 +1,22 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\EmailListController;
 
 Route::view('/', 'welcome');
 
 Route::view('/dashboard', 'dashboard')->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+    // Profile
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    // Email Lista
+    Route::get('/email-list', [EmailListController::class, 'index'])->name('email-list.index');
+    Route::get('/email-list/create', [EmailListController::class, 'create'])->name('email-list.create');
 });
 
 require __DIR__ . '/auth.php';
